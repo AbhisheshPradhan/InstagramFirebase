6f9a5825691036d8c3fc23240c848991ce650043
Abhishesh Pradhan <pradhan.abhishesh@gmail.com>
2018-04-14 15:46:07 +1000

    Ep6

---
+++ InstagramFirebase/Utils/Extension.swift
@@ -0,0 +1,51 @@
+//
+//  Extension.swift
+//  InstagramFirebase
+//
+//  Created by Abhishesh Pradhan on 11/4/18.
+//  Copyright © 2018 Abhishesh Pradhan. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIColor{
+    //static keyword allows to call the method on the class itself
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor{
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
+
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil,
+                paddingTop: CGFloat = 0,  paddingLeft: CGFloat = 0, paddingBot: CGFloat = 0, paddingRight: CGFloat = 0,
+                width: CGFloat = 0, height: CGFloat = 0
+        ){
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let bottom  = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBot).isActive = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+}
--- InstagramFirebase/ViewController.swift
+++ InstagramFirebase/ViewController.swift
@@ -7,19 +7,184 @@
 //
 
 import UIKit
+import Firebase
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
+    
+    let plusPhotoButton: UIButton = {
+        let button = UIButton (type: .system) //makes it white instead of blue when u click on it
+        
+        button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal) //render it to look like its original image
+        button.clipsToBounds = true //makes the bound of button into its actual size rather than a square containing it
+        
+        button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func handlePlusPhoto(){
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal  )
+        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width / 2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        plusPhotoButton.layer.borderWidth = 3
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        
+        tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
+        return tf
+    }()
+    
+    @objc func handleTextInputChange(){
+        //can also use !textfield.isEmpty or textfield.hasText or textfield.text?.count ?? 0 > 0
+        let isFormValid = emailTextField.hasText &&
+            usernameTextField.hasText &&
+            passwordTextField.hasText
+        
+        if isFormValid{
+            signUpButton.backgroundColor = UIColor.rgb(red: 17, green: 154, blue: 237)
+            signUpButton.isEnabled = true
+        }
+        else {
+            signUpButton.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
+            signUpButton.isEnabled = false
+        }
+    }
+    
+    let usernameTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Username"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
+        return tf
+    }()
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.isSecureTextEntry = true
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
+        return tf
+    }()
+    
+    let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        button.isEnabled = false
+        return button
+    }()
+    
+    @objc func handleSignUp(){
+        
+        guard let email = emailTextField.text, email.count > 0  else { return }
+        guard let username = usernameTextField.text, username.count > 0  else { return }
+        guard let password = passwordTextField.text, password.count > 0 else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) {(user, error) in
+            
+            if let err = error {
+                print("Failed to create user", err)
+                return
+            }
+            print("Successfully created User", user?.uid ?? "")
+            
+            guard let image = self.plusPhotoButton.imageView?.image else { return }
+            
+            guard let uploadData = UIImageJPEGRepresentation(image, 0.3) else { return }
+            
+            
+            let filename = NSUUID().uuidString
+            Storage.storage().reference().child("profile_images").child(filename).putData(uploadData, metadata: nil
+                , completion: { (metadata, err) in
+                    
+                    if let err = err {
+                        print("Failed to upload Profile image:", err)
+                        return
+                    }
+                    
+                    guard let profileImageUrl = metadata?.downloadURL()?.absoluteString else { return }
+                    
+                    print("Successfully uploaded Profile image:", profileImageUrl)
+                    
+                    guard let uid = user?.uid else { return }
+                    
+                    
+                    let dictionaryValues = ["username" : username, "profileImageUrl": profileImageUrl]
+                    let values = [uid: dictionaryValues]
+                    
+                    Database.database().reference().child("users").updateChildValues(values, withCompletionBlock: { (err, ref) in
+                        if let err = err {
+                            print("Failed to save user info into Database", err)
+                            return
+                        }
+                        print("Successfully saved user info into Database")
+                    })
+            })
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(plusPhotoButton)
+        
+        //Pragmatically positioning the plusPhotoButton
+        //AUTO LAYOUT CONSTRAINTS
+        
+        plusPhotoButton.anchor(top: view.topAnchor, paddingTop: 40, width: 140, height: 140)
+        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        setupInputFields()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    fileprivate func setupInputFields(){
+        //making stack view
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, usernameTextField, passwordTextField, signUpButton])
+        
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical //default axis is horizontal
+        stackView.spacing = 10
+        
+        view.addSubview(stackView)
+        
+        stackView.anchor(top: plusPhotoButton.bottomAnchor, left: view.leftAnchor,  right: view.rightAnchor, paddingTop: 20,  paddingLeft: 40, paddingRight: 40, height: 200)
+        
     }
-
-
 }
-
